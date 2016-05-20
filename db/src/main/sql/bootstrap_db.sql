ccd0d20f8148fba26d757d0667a7fc0f8db108a9
jza <juraj.zachar@celum.com>
2016-05-20 15:41:47 +0200

    cosmetic improvement to login form

--- db/src/main/sql/bootstrap_db.sql
+++ db/src/main/sql/bootstrap_db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE bilby_io_test;
 CREATE ROLE play WITH LOGIN PASSWORD 'play';
 -- \connect bilby_io_test
-GRANT ALL PRIVILEGES ON bilby_io_test TO manuel;
+GRANT ALL PRIVILEGES ON bilby_io_test TO play;
 CREATE EXTENSION hstore;
